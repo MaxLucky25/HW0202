6f9d328bef147de84a43aaab1305996719a4c163
MaksLucky25 <maksrad93@gmail.com>
2025-03-19 15:54:30 +0300

    add endpoint in testing

--- src/routers/testingRouter.ts
+++ src/routers/testingRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import {blogCollection, postCollection, userCollection} from "../db/mongo-db";
+import {blogCollection, commentCollection, postCollection, userCollection} from "../db/mongo-db";
 
 
 export const testingRouter = Router();
@@ -8,5 +8,6 @@ testingRouter.delete('/all-data', async (req, res) => {
     await blogCollection.deleteMany({});
     await postCollection.deleteMany({});
     await userCollection.deleteMany({});
+    await commentCollection.deleteMany({})
     res.sendStatus(204);
 });
